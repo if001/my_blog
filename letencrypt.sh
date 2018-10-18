a8dd2eeb4d487b9e3e1f06c04684d8ba358da237
IsseiFUJIMOTO <i.fujimoto@accelia.net>
2018-10-18 21:34:19 +0900

    ssl化 addし忘れ

---
+++ letencrypt.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+#証明書のドメイン（,区切りで複数のドメインも指定可能）
+DOMAIN=www.if-blog.site
+
+#ドキュメントルート（上のドメインで接続可能である必要がある）
+WEBROOT=/var/www/letsencrypt
+
+#メールアドレス（トラブル時にメールが届く）
+EMAIL=otomijuf.002@gmail.com
+
+sudo certbot certonly -m $EMAIL --agree-tos --non-interactive $* --webroot -w $WEBROOT -d $DOMAIN
+
+mv /etc/letsencrypt/live/${DOMAIN}/fullchain.pem nginx/key/fullchain.pem
+mv /etc/letsencrypt/live/${DOMAIN}/chain.pem     nginx/key/chain.pem 
+mv /etc/letsencrypt/live/example.com/privkey.pem nginx/key/privkey.pem 
+
+
